9f0c45d2849de250bbf9cf43576e5fc04a508d25
DineshDan <dineshdanchai341@gmail.com>
2020-11-24 06:35:20 +0530

    Modified: Logic to check the input of the three numbers

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,1 +1,10 @@
 echo "Welcome This problem computes different arithmetic expressions and sort the results"
+
+echo "Arithmetic Computation and Sorting"
+
+		read -p "enter a num: " num1
+		read -p "enter a num: " num2
+		read -p "enter a num: " num3
+a=$num1
+b=$num2
+c=$num3
