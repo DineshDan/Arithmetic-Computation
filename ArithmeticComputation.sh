0774b98ad489925d8eb76e1b530383af2d200f36
DineshDan <dineshdanchai341@gmail.com>
2020-11-24 07:10:03 +0530

    Modified: Logic to check to store the results in dictionary for every computation

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -9,12 +9,18 @@ b=$num2
 c=$num3
 
 echo "a+b*c=$(($a+$b*$c))"
-
-
 echo "a*b+c=$(($a*$b+$c))"
-
-
 echo "c+a/b=$(($c+$a/$b))"
-
-
 echo "a%b+c=$(($a%$b+$c))"
+
+		declare -A  arithmetic
+arithmetic[0]=$(($a+$b*$c))
+arithmetic[1]=$(($a*$b+$c))
+arithmetic[2]=$(($c+$a/$b))
+arithmetic[3]=$(($a%$b+$c))
+echo "a+b*c=${arithmetic[0]}" 
+echo "a*b+c=${arithmetic[1]}"
+echo "c+a/b=${arithmetic[2]}"
+echo "a%b+c=${arithmetic[3]}"
+echo "${arithmetic[@]}"
+
